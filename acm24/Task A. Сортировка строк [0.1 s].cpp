783f926350834332dc3ae03ebfa06de91540ebc2
kmelnikovmh <k.melnikov.mh@proton.me>
2025-04-15 16:45:10 +0300

    add acm24

---
+++ acm24/Task A. Сортировка строк [0.1 s].cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <random>
+#include <algorithm>
+
+std::string greedy_first(std::vector<std::string> str) {
+    std::sort(str.begin(), str.end(), [](std::string &lhs, std::string &rhs) {
+        return lhs < rhs;
+    });
+
+    std::string ans_str;
+    for (auto s : str) {
+        ans_str += s;
+    }
+    return ans_str;
+}
+
+std::string greedy_second(std::vector<std::string> str) {
+    std::sort(str.begin(), str.end(), [](std::string &lhs, std::string &rhs) {
+        return lhs + lhs < rhs + rhs;
+    });
+    
+    std::string ans_str;
+    for (auto s : str) {
+        ans_str += s;
+    }
+    return ans_str;
+}
+
+std::string right_greedy(std::vector<std::string> str) {
+    std::sort(str.begin(), str.end(), [](std::string &lhs, std::string &rhs) {
+        return lhs + rhs < rhs + lhs;
+    });
+    
+    std::string ans_str;
+    for (auto s : str) {
+        ans_str += s;
+    }
+    return ans_str;
+}
+
+int main() {
+    int type;
+    std::cin >> type;
+
+    std::mt19937 rng(229);
+    bool find_test = false;
+    while (!find_test) {
+        int n = 2 + rng() % (3 - 2 + 1);
+        std::vector<std::string> str(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j <= (1 + rng() % 3); ++j) {
+                bool type_str = rng() % 2;
+                if (type_str == 0) {
+                    str[i] += "a";
+                } else {
+                    str[i] += "b";
+                }
+            }
+        }
+
+        std::string ans_right_greedy = right_greedy(str);
+
+        std::string ans_lose_greedy;
+        if (type == 1) {
+            ans_lose_greedy = greedy_first(str);
+        } else {
+            ans_lose_greedy = greedy_second(str);
+        }
+
+        if (ans_right_greedy != ans_lose_greedy) {
+            find_test = true;
+
+            // std::cout << "\nlose : " << ans_lose_greedy << "\n";
+            // std::cout << "right : " << ans_right_greedy << "\n\n";
+
+            std::cout << n << "\n";
+            for (auto s : str) {
+                std::cout << s << " ";
+            }
+        }
+    }    
+}
---
+++ acm24/Task B. Пути нужной длины [0.2 s].cpp
@@ -0,0 +1,105 @@
+#include <vector>
+#include <iostream>
+#include "optimization.h"
+
+std::vector<std::vector<int>> graph(100'001);
+
+std::vector<int> deep(100'001, -1);
+std::vector<int> par_cen(100'001, -1);
+
+std::vector<std::pair<int, int64_t>> ans;
+
+std::vector<int64_t> freq(100'001, 0);
+int mx_depth = 0;
+
+// Очень сложная логика. Считаем не все ans, а только ans[L] как freq[L-curr_dist]
+void calc_dist(int v, int p, int depth, bool count_ans) {
+    mx_depth = std::max(mx_depth, depth);
+
+    if (count_ans) {
+        for (auto &pr : ans) {
+            int L = pr.first;
+            if (L >= depth) {
+                pr.second += freq[L - depth];
+            }
+        }
+    } else {
+        freq[depth]++;
+    }
+
+    for (int to : graph[v]) {
+        if (to != p && deep[to] == -1) {
+            calc_dist(to, v, depth + 1, count_ans);
+        }
+    }
+}
+
+void calc_ans(int centroid) {
+    mx_depth = 0;
+    freq[0] = 1;
+
+    for (int to : graph[centroid]) {
+        if (deep[to] != -1) continue;
+
+        calc_dist(to, centroid, 1, true);
+        calc_dist(to, centroid, 1, false);
+    }
+
+    for (int i = 0; i <= mx_depth; ++i) {
+        freq[i] = 0;
+    }
+}
+
+
+int get_centroid(int v, int par, int size_tree, int &centroid) {
+    int size = 1;
+
+    for (int to : graph[v]) {
+        if (to != par && deep[to] == -1) {
+            size += get_centroid(to, v, size_tree, centroid);
+        }
+    }
+
+    if (size * 2 >= size_tree && centroid == -1) {
+        centroid = v;
+    }
+
+    return size;
+}
+
+void build(int v, int par, int size_tree, int deepth) {
+    int centroid = -1;
+    get_centroid(v, -1, size_tree, centroid);
+    if (centroid == -1) centroid = v;
+    deep[centroid] = deepth; 
+    par_cen[centroid] = par;
+
+    calc_ans(centroid);
+    
+    for (int to : graph[centroid]) {
+        if (deep[to] == -1) {
+            build(to, centroid, (size_tree + 1) / 2, deepth + 1);
+        }
+    }
+}
+
+int main() {
+    int n = readInt();
+
+    for (int i = 1; i < n; ++i) {
+        int par = readInt();
+        graph[par].push_back(i);
+        graph[i].push_back(par);
+    }
+
+    int k = readInt();
+    for (int i = 0; i < k; ++i) {
+        ans.push_back({readInt(), 0});
+    }
+
+    build (0, -1, n, 0);
+
+    for (auto a : ans) {
+        writeInt(a.second, ' ');
+    }
+}
